5c9b6eed90cdf050cab4a244f778f8632ab22c30
Bian2017 <bgl_cumt_zju@163.com>
2020-11-02 18:56:41 +0800

    Chore: 解决Travis CI在部署时找不到js和css文件问题

--- .umirc.ts
+++ .umirc.ts
@@ -5,8 +5,9 @@ export default defineConfig({
   favicon: 'https://user-images.githubusercontent.com/9554297/83762004-a0761b00-a6a9-11ea-83b4-9c8ff721d4b8.png',
   logo: 'https://user-images.githubusercontent.com/9554297/83762004-a0761b00-a6a9-11ea-83b4-9c8ff721d4b8.png',
   outputPath: 'docs-dist',
-  publicPath: '/ii-admin-charts',
+  publicPath: '/ii-admin-charts/',
   base: '/ii-admin-charts',
   mode: 'site',
+  history:  { type: 'hash'}
   // more config: https://d.umijs.org/config
 });
