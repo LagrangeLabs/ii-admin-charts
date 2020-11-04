cb0f0b16f4e7ffd355b9cb705878e4d5af67dbc0
Bian2017 <bgl_cumt_zju@163.com>
2020-11-04 14:31:55 +0800

    style: 删除dumi自动生成的组件

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,3 @@
 export { default as Doubleaxes } from './Doubleaxes';
 export { default as IDonutChart } from './IDonutChart';
 export { default as IDoublePieChart } from './IDoublePieChart';
-export { default as Foo } from './Foo';
